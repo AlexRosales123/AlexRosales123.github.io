5828f830f0658006d5c4bd1fb3356d6a549f1ad2
AlexRosales123 <67491344+AlexRosales123@users.noreply.github.com>
2021-01-19 03:54:33 +0000

    Deploying to gh-pages from  @ 038d5aaedc8e21b992d791429abe47b98b30bf8d 🚀

---
+++ _next/static/0OTAGdJobDYvZdaHMTMiv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Ff833cae39500b8148671.css","static\u002Fchunks\u002Fpages\u002Findex-e6849a971e77ee33f3a9.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
